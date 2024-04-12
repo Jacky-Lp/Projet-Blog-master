2e47357618376859c1e915ce7d5d3485e1fd1f1c
Jacky-Lp <121239076+Jacky-Lp@users.noreply.github.com>
2024-04-12 10:02:58 +0200

    modif page

--- app/accueil/page.tsx
+++ app/accueil/page.tsx
@@ -2,7 +2,7 @@ export default function Accueil() {
     return (
         <body>
             <main>
-                <h1>ZIZI</h1>
+                <h1>ZIZI fsgfdsgfdsgfdsgfdsgfdd</h1>
             </main>
         </body>
     );
